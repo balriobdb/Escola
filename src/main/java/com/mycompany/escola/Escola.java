c3f5950dfe84831d9f26d949fb6885c6e9e55420
mauriciofreitasb3@gmail.com <>
2025-09-15 19:47:35 -0300

    Inicializado repositório Escola

---
+++ src/main/java/com/mycompany/escola/Escola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escola;
+
+/**
+ *
+ * @author mauricio.freitas1
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
